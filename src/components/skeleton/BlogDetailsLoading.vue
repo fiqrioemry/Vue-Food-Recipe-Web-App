44de3d88b6994ea02964c92124184b202eb7d5ea
fiqrioemry <foemry@gmail.com>
2024-12-07 23:58:18 +0700

    add animation

--- src/components/skeleton/BlogDetailsLoading.vue
+++ src/components/skeleton/BlogDetailsLoading.vue
@@ -1,5 +1,61 @@
-<template lang="">
-  <div></div>
+<template>
+  <div class="section__wrapper">
+    <!-- Page Navigation -->
+    <div class="h-8 w-24 rounded-md bg-slate-200 animate-pulse"></div>
+
+    <div class="space-y-4 py-6 border-b">
+      <div class="h-8 w-24 rounded-md bg-slate-200 animate-pulse"></div>
+
+      <div class="flex items-center gap-x-6">
+        <div class="h-8 w-24 rounded-md bg-slate-200 animate-pulse"></div>
+        <div class="h-8 w-24 rounded-md bg-slate-200 animate-pulse"></div>
+      </div>
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-5 gap-x-6">
+      <div class="col-span-1 md:col-span-3 space-y-4">
+        <div class="space-y-4">
+          <div
+            class="w-full h-[400px] rounded-md bg-slate-200 animate-pulse"
+          ></div>
+
+          <div>
+            <div class="h-8 w-24 rounded-md bg-slate-200 animate-pulse"></div>
+            <div class="h-8 w-full rounded-md bg-slate-200 animate-pulse"></div>
+            <div class="h-8 w-full rounded-md bg-slate-200 animate-pulse"></div>
+            <div class="h-8 w-full rounded-md bg-slate-200 animate-pulse"></div>
+            <div class="h-8 w-1/2 rounded-md bg-slate-200 animate-pulse"></div>
+          </div>
+          <div>
+            <div class="h-8 w-24 rounded-md bg-slate-200 animate-pulse"></div>
+            <div class="h-8 w-full rounded-md bg-slate-200 animate-pulse"></div>
+            <div class="h-8 w-full rounded-md bg-slate-200 animate-pulse"></div>
+            <div class="h-8 w-full rounded-md bg-slate-200 animate-pulse"></div>
+            <div class="h-8 w-1/2 rounded-md bg-slate-200 animate-pulse"></div>
+          </div>
+        </div>
+
+        <div class="h-8 w-1/3 rounded-md bg-slate-200 animate-pulse"></div>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div>
+            <div
+              class="w-full h-[225px] rounded-md bg-slate-200 animate-pulse"
+            ></div>
+            <div class="h-8 w-20 rounded-md bg-slate-200 animate-pulse"></div>
+          </div>
+          <div>
+            <div
+              class="w-full h-[225px] rounded-md bg-slate-200 animate-pulse"
+            ></div>
+            <div class="h-8 w-20 rounded-md bg-slate-200 animate-pulse"></div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Aside Content -->
+      <aside></aside>
+    </div>
+  </div>
 </template>
 <script>
 export default {};
