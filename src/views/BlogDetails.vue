93653eeed7f3f3652e759c7195f01ea50f30ed95
fiqrioemry <foemry@gmail.com>
2024-12-07 23:30:57 +0700

    add related blog

--- src/views/BlogDetails.vue
+++ src/views/BlogDetails.vue
@@ -10,7 +10,7 @@
         {{ blog.title }}
       </div>
 
-      <!-- Blog Author & date -->
+      <!-- author & date -->
       <div>
         <div class="flex items-center">
           <div
@@ -28,33 +28,53 @@
     <!-- blog body -->
     <div class="grid grid-cols-1 md:grid-cols-5 gap-x-6">
       <!-- main content -->
-      <article class="col-span-1 md:col-span-3 space-y-4">
-        <div class="image__wrapper h-[400px]">
-          <img class="w-full" :src="blog.image" alt="blog_thumbnail" />
-        </div>
-        <p class="font-medium">{{ blog.excerpt }}</p>
-        <div class="space-y-4">
-          <p
-            v-for="(text, index) in blog.content"
-            :key="index"
-            class="text-justify"
-          >
-            {{ text }}
-          </p>
-        </div>
-        <div class="flex items-center gap-x-2">
-          <h6>Tags :</h6>
+      <div class="col-span-1 md:col-span-3 space-y-4">
+        <article class="space-y-4">
+          <!-- blog image -->
+          <div class="image__wrapper h-[400px]">
+            <img class="w-full" :src="blog.image" alt="blog_thumbnail" />
+          </div>
+          <!-- blog excerpt -->
+          <p class="font-medium">{{ blog.excerpt }}</p>
+          <div class="space-y-4">
+            <!-- blog paragraph -->
+            <p
+              v-for="(text, index) in blog.content"
+              :key="index"
+              class="text-justify"
+            >
+              {{ text }}
+            </p>
+          </div>
+          <!-- blog tags -->
+          <div class="flex items-center gap-x-2">
+            <h6>Tags :</h6>
+            <div
+              v-for="(tag, index) in blog.tags"
+              :key="index"
+              class="py-1 px-2 border rounded-md text-sm"
+            >
+              {{ tag }}
+            </div>
+          </div>
+        </article>
+
+        <!-- another related blog -->
+        <div class="grid grid-cols-2 gap-4">
           <div
-            v-for="(tag, index) in blog.tags"
+            v-for="(relatedBlog, index) in relatedBlogs"
             :key="index"
-            class="py-1 px-2 border rounded-md text-sm"
+            class="related-blog"
           >
-            {{ tag }}
+            <div class="image__wrapper">
+              <img :src="relatedBlog.image" alt="another_blog" />
+            </div>
+            <h6 class="font-bold">{{ relatedBlog.title }}</h6>
           </div>
         </div>
-      </article>
+      </div>
 
-      <!-- side content -->
+      <!-- aside content -->
       <aside></aside>
     </div>
   </section>
@@ -68,17 +88,28 @@ export default {
   name: "BlogDetails",
   props: ["id"],
   data() {
-    return { blogInfoSummary, data: [], articles, loading: true };
+    return {
+      blogInfoSummary,
+      data: [],
+      articles,
+      relatedBlogs: [],
+      loading: true,
+    };
   },
   methods: {
     fetchBlogById(id) {
       setTimeout(() => {
-        const result = this.articles.filter((item) => item.id === parseInt(id));
-        this.data = result;
+        const result = this.articles.find((item) => item.id === parseInt(id));
+        this.data = result ? [result] : [];
+        this.relatedBlogs = this.fetchRelatedBlogs(parseInt(id));
         this.loading = false;
       }, 500);
     },
+    fetchRelatedBlogs(currentBlogId) {
+      return this.articles.filter((article) => article.id !== currentBlogId);
+    },
   },
+
   mounted() {
     this.fetchBlogById(this.$route.params.id);
   },
