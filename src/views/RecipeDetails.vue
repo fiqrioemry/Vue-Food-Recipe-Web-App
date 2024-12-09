af4c316df57120a9f35b2b01482da748053974c3
fiqrioemry <foemry@gmail.com>
2024-12-09 22:43:09 +0700

    add section recipe details

--- src/views/RecipeDetails.vue
+++ src/views/RecipeDetails.vue
@@ -137,40 +137,44 @@
           <!-- you might also like -->
           <div class="space-y-6 py-6">
             <h4>You might also like</h4>
-            <div class="grid grid-cols-3 gap-x-4 gap-y-4">
+
+            <div v-if="loading">Loading...</div>
+            <div v-else class="grid grid-cols-3 gap-x-4 gap-y-4">
               <article
                 v-for="recommend in recommendRecipes"
                 class="rounded-md shadow-lg"
               >
-                <div class="image__wrapper relative">
-                  <button
-                    class="absolute right-2 top-2 bg-background px-4 py-2 rounded-md"
-                  >
-                    <font-awesome-icon
-                      class="text-accent text-xs"
-                      icon="fa-solid fa-bookmark"
+                <RouterLink :to="`/recipe`" :key="related.id">
+                  <div class="image__wrapper relative">
+                    <button
+                      class="absolute right-2 top-2 bg-background px-4 py-2 rounded-md"
+                    >
+                      <font-awesome-icon
+                        class="text-accent text-xs"
+                        icon="fa-solid fa-bookmark"
+                      />
+                    </button>
+                    <img
+                      class="w-full h-[200px] rounded-t-md"
+                      :src="recommend.image"
+                      alt="favorite"
                     />
-                  </button>
-                  <img
-                    class="w-full h-[200px] rounded-t-md"
-                    :src="recommend.image"
-                    alt="favorite"
-                  />
-                </div>
+                  </div>
 
-                <div class="px-2 py-2 min-h-[75px]">
-                  <div>
-                    <font-awesome-icon
-                      v-for="i in 5"
-                      class="text-accent text-xs"
-                      icon="fa-solid fa-star"
-                      :key="i"
-                    />
+                  <div class="px-2 py-2 min-h-[75px]">
+                    <div>
+                      <font-awesome-icon
+                        v-for="i in 5"
+                        class="text-accent text-xs"
+                        icon="fa-solid fa-star"
+                        :key="i"
+                      />
+                    </div>
+                    <div class="text-xs md:text-sm">
+                      {{ recommend.name }}
+                    </div>
                   </div>
-                  <div class="text-xs md:text-sm">
-                    {{ recommend.name }}
-                  </div>
-                </div>
+                </RouterLink>
               </article>
             </div>
           </div>
@@ -214,12 +218,13 @@
           <!-- related recipe -->
           <div>
             <h5>Related Recipes</h5>
-            <div class="grid grid-cols-2 gap-x-3 gap-y-3">
+            <div v-if="loading">Loading...</div>
+            <div v-else class="grid grid-cols-2 gap-x-3 gap-y-3">
               <article
                 v-for="related in relatedRecipes"
                 class="rounded-md shadow-lg"
               >
-                <RouterLink :to="`/recipe/${related.id}}`" :key="related.id">
+                <RouterLink :to="`/recipe/${related.name}`" :key="related.id">
                   <div class="image__wrapper">
                     <img
                       class="w-full h-[150px] rounded-t-md"
@@ -242,6 +247,7 @@
     </section>
   </main>
 </template>
+
 <script>
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -256,37 +262,44 @@ export default {
     Input,
   },
   setup() {
-    const route = useRoute(); // Ambil route saat ini
-    const slug = ref(route.params.slug); // Ambil parameter 'slug'
+    const route = useRoute();
+    const slug = ref(route.params.slug);
 
     const nutrisionFactData = nutrisionFact;
     const recipeDetailData = recipeDetail;
-
     const relatedRecipes = ref([]);
     const recommendRecipes = ref([]);
     const recipes = ref([]);
     const loading = ref(true);
 
     const fetchData = async (slug) => {
-      const response = await fetch(`https://dummyjson.com/recipes/${slug}`);
+      loading.value = true;
+      try {
+        const response = await fetch(
+          `https://dummyjson.com/recipes/search?q=${slug}`
+        );
+        const result = await response.json();
+        recipes.value = result.recipes;
 
-      const result = await response.json();
-      console.log(result);
-      recipes.value = result;
+        const tags = recipes.value[0].tags[0];
+        const mealType = recipes.value[0].mealType[0];
 
-      const tags = recipes.value[0].tags[0];
+        const recommend = await fetch(
+          `https://dummyjson.com/recipes/meal-type/${mealType}`
+        );
+        const recommendData = await recommend.json();
+        recommendRecipes.value = recommendData.recipes.slice(0, 6);
 
-      const mealType = recipes.value[0].mealType[0];
-
-      const recommend = await fetch(
-        `https://dummyjson.com/recipes/meal-type/${mealType}`
-      );
-      const recommendData = await recommend.json();
-      recommendRecipes.value = recommendData.recipes.slice(0, 6);
-
-      const related = await fetch(`https://dummyjson.com/recipes/tag/${tags}`);
-      const relatedData = await related.json();
-      relatedRecipes.value = relatedData.recipes;
+        const related = await fetch(
+          `https://dummyjson.com/recipes/tag/${tags}`
+        );
+        const relatedData = await related.json();
+        relatedRecipes.value = relatedData.recipes;
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        loading.value = false;
+      }
     };
 
     onMounted(() => {
