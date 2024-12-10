15618d1c440e8fb96efb771a06299c3f0c926a4d
fiqrioemry <foemry@gmail.com>
2024-12-10 15:05:20 +0700

    add pagination for recipe

--- src/views/Recipe.vue
+++ src/views/Recipe.vue
@@ -17,15 +17,17 @@
       <div class="text-center">
         <h2>What to <span class="text-accent">Cook</span>?</h2>
 
-        <Tabs :default-value="categories">
+        <Tabs :default-value="'all'">
           <TabsList class="flex flex-wrap gap-x-4 gap-y-4">
+            <!-- All Categories Tab -->
             <TabsTrigger
-              :value="categories"
+              value="all"
               class="rounded-full px-6"
-              @click="handleTabClick(categories)"
+              @click="handleTabClick('all')"
             >
-              All categories
+              All Categories
             </TabsTrigger>
+            <!-- Dynamic Categories Tabs -->
             <TabsTrigger
               v-for="(category, index) in categories"
               :key="index"
@@ -36,27 +38,41 @@
               {{ category }}
             </TabsTrigger>
           </TabsList>
+          <!-- Dynamic Categories Content -->
+          <TabsContent value="all" class="space-y-6">
+            <div class="grid grid-cols-2 md:grid-cols-4 gap-x-4 gap-y-4">
+              <article v-for="(recipe, index) in recipes" :key="index">
+                <div class="image__wrapper">
+                  <img :src="recipe.image" alt="image_recipes" />
+                </div>
+              </article>
+            </div>
+            <!-- Pagination -->
+            <BlogPagination
+              :currentPage="currentPage"
+              :totalPages="totalPages"
+              :onPageChanged="onPageChanged"
+            />
+          </TabsContent>
           <TabsContent
             v-for="(category, index) in categories"
             :key="index"
             :value="category"
+            class="space-y-6"
           >
-            <div class="grid grid-cols-4 gap-x-4 gap-y-4">
+            <div class="grid grid-cols-2 md:grid-cols-4 gap-x-4 gap-y-4">
               <article v-for="(recipe, index) in recipes" :key="index">
                 <div class="image__wrapper">
                   <img :src="recipe.image" alt="image_recipes" />
                 </div>
               </article>
             </div>
-          </TabsContent>
-          <TabsContent :value="categories">
-            <div class="grid grid-cols-4 gap-x-4 gap-y-4">
-              <article v-for="(recipe, index) in recipes" :key="index">
-                <div class="image__wrapper">
-                  <img :src="recipe.image" alt="image_recipes" />
-                </div>
-              </article>
-            </div>
+            <!-- Pagination -->
+            <BlogPagination
+              :currentPage="currentPage"
+              :totalPages="totalPages"
+              :onPageChanged="onPageChanged"
+            />
           </TabsContent>
         </Tabs>
       </div>
@@ -66,20 +82,26 @@
 
 <script>
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-
+import BlogPagination from "@/components/blog/BlogPagination.vue";
 export default {
   components: {
     Tabs,
     TabsContent,
     TabsList,
     TabsTrigger,
+    BlogPagination,
   },
   data() {
     return {
       loading: true,
       categories: [],
-      mealType: [],
       recipes: [],
+      mealType: "",
+      totalPages: 0,
+      firstIndex: 0,
+      lastIndex: 0,
+      dataPerPage: 4,
+      currentPage: 1,
     };
   },
   methods: {
@@ -87,8 +109,11 @@ export default {
       try {
         const response = await fetch("https://dummyjson.com/recipes?limit=100");
         const { recipes } = await response.json();
+
         const mealType = [...new Set(recipes.flatMap((item) => item.mealType))];
         this.categories = mealType;
+
+        this.handleTabClick("all");
       } catch (error) {
         console.error("Error fetching meal types:", error);
       }
@@ -96,20 +121,26 @@ export default {
 
     async fetchByMealType(mealType) {
       try {
-        if (typeof mealType === "object" || !mealType.length) {
+        if (mealType === "all") {
+          this.mealType = mealType;
           const response = await fetch(
             "https://dummyjson.com/recipes?limit=100"
           );
           const { recipes } = await response.json();
-          this.recipes = recipes;
-          console.log(this.recipes);
+          this.totalPages = Math.ceil(recipes.length / this.dataPerPage);
+          this.firstIndex = (this.currentPage - 1) * this.dataPerPage;
+          this.lastIndex = this.firstIndex + this.dataPerPage;
+          this.recipes = recipes.slice(this.firstIndex, this.lastIndex);
         } else {
           const response = await fetch(
             `https://dummyjson.com/recipes/meal-type/${mealType}`
           );
           const { recipes } = await response.json();
-          this.recipes = recipes;
-          console.log(this.recipes);
+          this.totalPages = Math.ceil(recipes.length / this.dataPerPage);
+          this.firstIndex = (this.currentPage - 1) * this.dataPerPage;
+          this.lastIndex = this.firstIndex + this.dataPerPage;
+          this.recipes = recipes.slice(this.firstIndex, this.lastIndex);
+          this.mealType = mealType;
         }
       } catch (error) {
         console.error("Error fetching recipes by meal type:", error);
@@ -117,13 +148,16 @@ export default {
     },
 
     handleTabClick(selectedMealType) {
-      this.mealType = selectedMealType;
       this.fetchByMealType(selectedMealType);
+    },
+
+    onPageChanged(page) {
+      this.currentPage = page;
+      this.fetchByMealType(this.mealType);
     },
   },
   mounted() {
     this.fetchAllMealType();
-    this.fetchByMealType(this.mealType);
   },
 };
 </script>
