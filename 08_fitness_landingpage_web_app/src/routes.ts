39cbc6209fc64cea8ee2578261dab0ef3e388240
Ahmad Fiqri Oemry <121015122+fiqrioemry@users.noreply.github.com>
2025-03-27 12:38:38 +0700

    Delete 08_fitness_landingpage_web_app directory

--- 08_fitness_landingpage_web_app/src/routes.ts
+++
@@ -1,9 +0,0 @@
-import Home from "./pages/Home.svelte";
-import Login from "./pages/Login.svelte";
-import Register from "./pages/Register.svelte";
-
-export default {
-  "/": Home,
-  "/login": Login,
-  "/register": Register,
-};
